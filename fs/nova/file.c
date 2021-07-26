e73e01376c26e3ae8fe5d261c15241bb6aa0580f
yonghyeon <yhcho.raphael@gmail.com>
2021-07-26 16:47:48 +0900

    implementing a SHA1 example in write path

--- fs/nova/file.c
+++ fs/nova/file.c
@@ -23,11 +23,17 @@
 #include "nova.h"
 #include "inode.h"
 
+/*** DEDUP ***/
+#include <crypto/hash.h>
+#include <crypto/skcipher.h>
+#include <linux/crypto.h>
+
 // DEDUP //
 static int nova_dedup(struct file *filp) {
 	printk("Dedup Function Called \n");
 	return 7;
 }
+static int test_hash(const unsigned char *data, unsigned int datalen, unsigned char *digest);
 // ----- //
 
 static inline int nova_can_set_blocksize_hint(struct inode *inode,
@@ -657,7 +663,8 @@ static ssize_t do_nova_cow_file_write(struct file *filp,
 	u64 epoch_id;
 	u32 time;
 	unsigned long irq_flags = 0;
-
+//DEDUP//
+unsigned char digest[40];
 
 	if (len == 0)
 		return 0;
@@ -717,7 +724,8 @@ static ssize_t do_nova_cow_file_write(struct file *filp,
 	update.alter_tail = sih->alter_log_tail;
 	while (num_blocks > 0) {		// [yhc] 남아있는 block이 있다면,
 		printk("NOVAAA : helloo\n");	// [yhc] mnt시 dmesg로 확인.
-
+test_hash("hello dedup nova", 16, digest);
+printk("***Dedup Hash: test_hash() -> %s \n", digest);	// hash testing.
 		offset = pos & (nova_inode_blk_size(sih) - 1);
 		start_blk = pos >> sb->s_blocksize_bits;
 
@@ -1046,3 +1054,60 @@ const struct inode_operations nova_file_inode_operations = {
 	.getattr	= nova_getattr,
 	.get_acl	= NULL,
 };
+/*** DEDUP ***/
+struct sdesc {
+    struct shash_desc shash;
+    char ctx[];
+};
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg)
+{
+    struct sdesc *sdesc;
+    int size;
+
+    size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+    sdesc = kmalloc(size, GFP_KERNEL);
+    if (!sdesc)
+        return ERR_PTR(-ENOMEM);
+    sdesc->shash.tfm = alg;
+    sdesc->shash.flags = 0x0;
+    return sdesc;
+}
+
+static int calc_hash(struct crypto_shash *alg,
+             const unsigned char *data, unsigned int datalen,
+             unsigned char *digest)
+{
+    struct sdesc *sdesc;
+    int ret;
+
+    sdesc = init_sdesc(alg);
+    if (IS_ERR(sdesc)) {
+        pr_info("can't alloc sdesc\n");
+        return PTR_ERR(sdesc);
+    }
+
+    ret = crypto_shash_digest(&sdesc->shash, data, datalen, digest);
+    kfree(sdesc);
+    return ret;
+}
+
+/*
+Test function, data is the data for hash, datalen is the data length, and digest is the sha1 result (is an out variable)
+*/
+static int test_hash(const unsigned char *data, unsigned int datalen,
+             unsigned char *digest)
+{
+    struct crypto_shash *alg;
+    char *hash_alg_name = "sha1";
+    int ret;
+
+    alg = crypto_alloc_shash(hash_alg_name, 0, 0);
+    if (IS_ERR(alg)) {
+            pr_info("can't alloc alg %s\n", hash_alg_name);
+            return PTR_ERR(alg);
+    }
+    ret = calc_hash(alg, data, datalen, digest);
+    crypto_free_shash(alg);
+    return ret;
+}
